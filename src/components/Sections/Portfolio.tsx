ac596ab4739373ae96b34d2f856f91dc5377fd07
Eric Dacal Sánchez <ee.edacal@ficosa.com>
2023-10-24 10:48:25 +0200

    feat: Implement a template for Portfolio page and remove unnused images

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -1,13 +1,20 @@
 import {FC, memo} from 'react';
+import { useRouter } from 'next/router';
 
 import {SectionId} from '../../data/data';
 import Section from '../Layout/Section';
 
 const Portfolio: FC = memo(() => {
+  const router = useRouter();
+
+  const handleButtonClick = () => {
+    router.push("/portfolio");
+  };
+
   return (
     <Section className="bg-neutral-800" sectionId={SectionId.Portfolio}>
       <div className="flex justify-center items-center h-screen">
-        <button className="bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded">
+        <button className="bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded"  onClick={handleButtonClick}>
           Access to Portfolio
         </button>
       </div>
---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,22 @@
+function Portfolio() {
+	return (
+	  <div>
+		PortfolioPage
+		{/*<h1>My Unity WebGL Portfolio</h1>
+		
+		<div id="unityContainer" style={{ width: "960px", height: "600px", margin: "auto", backgroundColor: "#000000" }}>
+		  <canvas className="emscripten" id="canvas" onContextMenu={(event) => event.preventDefault()}></canvas>
+		  <br />
+		</div>
+  
+		<script src="/unityWebGL/UnityLoader.js"></script>
+		<script>
+		  {`
+			var gameInstance = UnityLoader.instantiate("unityContainer", "/unityWebGL/Build.json", { onProgress: UnityProgress });
+		  `}
+		</script>*/}
+	  </div>
+	);
+  }
+  export default Portfolio;
+  
