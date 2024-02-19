bc22c19b0e6045feae1f040015070c71dff0d1a1
f36eds0 <ee.edacal@ficosa.com>
2024-02-19 16:04:48 +0100

    feat: Add a aesthetic loading screen base in 80s games

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -5,10 +5,10 @@ import {RoomEnvironment} from 'three/examples/jsm/environments/RoomEnvironment.j
 
 const Portfolio: FC = memo(() => {
   const mountRef = useRef<HTMLDivElement | null>(null);
-  // State to manage the loading screen visibility
   const [isLoading, setIsLoading] = useState(true);
   const isOrbitingRef = useRef(true);
-  
+
+  /////// HTML HANDLERS ///////
    // Handler for the left arrow click
   const handleLeftArrowClick = () => {
     console.log('Left arrow clicked');
@@ -22,15 +22,93 @@ const Portfolio: FC = memo(() => {
     // Implement what happens when the right arrow is clicked
     // For example, rotate the scene, move the camera, etc.
   };
-
+  ////////////////////////////
+  
+  
   useEffect(() => {
+    
+    
+    /////// THREE.JS related EVENTS ///////
     const onClick = () => {
       // Call the function to animate the camera
       animateCameraToFrontView();
     }
+    function onWindowResize() {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+  
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+  
+      renderer.setSize(width, height);
+      // If you're using a composer, uncomment the next line
+      // composer.setSize(width, height);
+    }
+    ////////////////////////////
+    
+    /////// SCENE SETUP ///////
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    ////////////////////////////
+   
 
+    
+    /////// RENDERER SETUP ///////
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.toneMapping = THREE.ReinhardToneMapping;
+
+    const pmremGenerator = new THREE.PMREMGenerator(renderer);
+    scene.environment = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture;
+    ////////////////////////////
+
+
+
+    /////// EQUIRECTANGULAR BACKGROUND ///////
+    const textureLoader = new THREE.TextureLoader();
+    textureLoader.load('background-scene.png', function(texture) {
+      const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
+      rt.fromEquirectangularTexture(renderer, texture);
+      scene.background = rt.texture;
+    });
+    ////////////////////////////
+
+    
+    if (mountRef.current) {
+      mountRef.current.appendChild(renderer.domElement);
+      mountRef.current.addEventListener('click', onClick);
+    }
+    window.addEventListener('resize', onWindowResize);
+    
+
+
+    const loader = new THREE.ObjectLoader();
+    loader.load(
+      'scene.json', // The path to your exported scene file
+      function (object) {
+        scene.add(object);
+        //controls.target.copy(object.position);
+        //controls.update();
+        // Hide the loading screen once the scene is fully loaded
+        setTimeout(() => {
+          setIsLoading(false); // Hide loader after loading is complete
+        }, 20000); 
+      },
+      function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+      },
+      function (error) {
+        console.log('An error happened', error);
+      }
+    );
+    // Camera position
+    camera.position.set(0, 0.2, 1);
+
+
+
+    /////// ANIMATIONS ///////
     const animateCameraToFrontView = () => {
-      
       // Camera Config
       isOrbitingRef.current = false; 
       const frontViewPosition = new THREE.Vector3(0, 0.75, 0.25); // Example position in front of the object
@@ -41,9 +119,7 @@ const Portfolio: FC = memo(() => {
     
       const initialPosition = camera.position.clone();
       const initialQuaternion = camera.quaternion.clone();
-      const targetQuaternion = new THREE.Quaternion().setFromEuler(new THREE.Euler(0, 0, 0)); // Front view orientation
-      
-
+      const targetQuaternion = new THREE.Quaternion().setFromEuler(new THREE.Euler(0, 0, 0)); // Front view orientatio
 
       function animate() {
         const elapsedTime = performance.now() - startTime;
@@ -66,65 +142,17 @@ const Portfolio: FC = memo(() => {
     
       animate();
     };
-    // Scene setup
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    const radius = 1; // Example value, adjust as needed
     let angle = 0; // Initial angle
-    const height = 1; // Height from the base, creates a diagonal angle
     let angleIncrement = 0.001; // Speed of the orbit
-    const minAngle = 0; // Minimum angle, could be 0 or any other value
-    const maxAngle = Math.PI/2; // Maximum angle, half orbit, adjust as needed
 
-    camera.position.set(radius * Math.sin(angle), height, radius * Math.cos(angle));
-    camera.lookAt(scene.position);
-
-   
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.toneMapping = THREE.ReinhardToneMapping;
-    if (mountRef.current) {
-      mountRef.current.appendChild(renderer.domElement);
-      mountRef.current.addEventListener('click', onClick);
-    }
-    const pmremGenerator = new THREE.PMREMGenerator(renderer);
-    scene.environment = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture;
-
-
-      // Equirectangular background
-    const textureLoader = new THREE.TextureLoader();
-    textureLoader.load('background-scene.png', function(texture) {
-      const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
-      rt.fromEquirectangularTexture(renderer, texture);
-      scene.background = rt.texture;
-    });
-
-
-    const loader = new THREE.ObjectLoader();
-    loader.load(
-      'scene.json', // The path to your exported scene file
-      function (object) {
-        scene.add(object);
-        //controls.target.copy(object.position);
-        //controls.update();
-        // Hide the loading screen once the scene is fully loaded
-        setIsLoading(false);
-      },
-      function (xhr) {
-        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-      },
-      function (error) {
-        console.log('An error happened', error);
-      }
-    );
-    // Camera position
-    camera.position.set(0, 0.2, 1);
-
-    // Animation loop (Orbit around center object)
-    const animate = function () {
-      requestAnimationFrame(animate);
+    const animateOrbitCamera = function () {
+      requestAnimationFrame(animateOrbitCamera);
+      const radius = 1; // Example value, adjust as needed
+      const height = 1; // Height from the base, creates a diagonal angle
+      const minAngle = 0; // Minimum angle, could be 0 or any other value
+      const maxAngle = Math.PI/2; // Maximum angle, half orbit, adjust as needed
+  
     
       if (isOrbitingRef.current) {
         // Update the angle for the orbit
@@ -143,13 +171,21 @@ const Portfolio: FC = memo(() => {
     
       renderer.render(scene, camera);
     };
-    animate();
+    ////////////////////////////
+
+    animateOrbitCamera();
   }, []);
 
   return (
     <div>
       {/* Loading screen element */}
-      {isLoading && <div className="loading-screen">Loading...</div>}
+      {isLoading && (
+        <div className={`loader-screen ${!isLoading ? 'fade-out' : ''}`}>
+          <h2>Portfolio</h2>
+          <div className="slider"></div>
+        </div>
+      )}
+
 
       {/* The container for the Three.js scene */}
       <div className="portfolio-container" ref={mountRef}></div>
