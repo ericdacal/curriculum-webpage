bfc1a27ce59119f346aae521f44bda4e7683db89
Eric <eric@dacal.es>
2024-07-19 15:16:56 +0200

    feat: new slides in Starfall project

--- src/pages/starfall.tsx
+++ src/pages/starfall.tsx
@@ -9,7 +9,7 @@ const Starfall: FC = memo(() => {
           <img alt="Presentation slide" src="slides/starfall/1.png"></img>
           <Carousel.Caption>
             <h1>Starfall Rebellion</h1>
-            <p>Master's Degree Project, where we develop a  Lego Star Wars game<a href="https://starfall-rebellion.vercel.app/" target="_blank">(Web)</a></p>
+            <p>Master's Degree Project, where we develop a  Lego Star Wars game<a href="https://starfall-rebellion.vercel.app/" target="_blank">(Web)</a><a href="https://github.com/Horizons-Games/Axolotl-Engine" target="_blank">(Github)</a></p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -24,7 +24,7 @@ const Starfall: FC = memo(() => {
           <Carousel.Caption>
             <h1>Character Concept & Design</h1>
             <p>I conceptualize and design one of the main characters Bix</p>
-          </Carousel.Caption>
+          </Carousel.Caption>  
         </Carousel.Item>
         <Carousel.Item>
           <img alt="Third slide" src="slides/starfall/slide3.png"></img>
@@ -34,14 +34,44 @@ const Starfall: FC = memo(() => {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img alt="Fourth slide" src="slides/starfall/slide0.webp"></img>
+          <img alt="Fourth slide" src="slides/starfall/slide4.png"></img>
+          <Carousel.Caption>
+            <h1>Props Modelling & Texturing</h1>
+            <p>I model and texture some props and enviroment models</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img alt="Fifth slide" src="slides/starfall/slide0.webp"></img>
           <Carousel.Caption>
             <h1>Second Level</h1>
             <p>Desing, modelling and animation second level items</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img alt="Fifth slide" src="slides/starfall/slide0.webp"></img>
+          <div className="carousel-background">
+            <img alt="Fifth slide" src="slides/starfall/slide_base.png" className="background-image"></img>
+            <img alt="Character 1" src="slides/starfall/bith_animation.gif" className="overlay-gif gif1"></img>
+            <img alt="Character 2" src="slides/starfall/human_animation.gif" className="overlay-gif gif2"></img>
+            <img alt="Character 2" src="slides/starfall/bix_animation.gif" className="overlay-gif gif3"></img>
+          </div>
+          <Carousel.Caption>
+            <h1>Animated Characters</h1>
+            <p>Creation of animation for Character</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="carousel-background">
+            <img alt="Sixth slide" src="slides/starfall/slide_base.png" className="background-image"></img>
+            <img alt="Bix VFX" src="slides/starfall/bix_vfx.gif" className="overlay-gif gif1"></img>
+            <img alt="Allura VFX" src="slides/starfall/allura_vfx.gif" className="overlay-gif gif2"></img>
+          </div>
+          <Carousel.Caption>
+            <h1>VFX for main character</h1>
+            <p>Creation and design of VFX for some skill of main characters</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img alt="Seventh slide" src="slides/starfall/slide0.webp"></img>
           <Carousel.Caption>
             <h1>Other Models</h1>
             <p>Another models</p>
